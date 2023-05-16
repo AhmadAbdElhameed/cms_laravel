eef592c9620d23b1fdd248825323390ade7a3425
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-05-16 03:20:16 +0300

    create blog tables

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         Mindscms\Entrust\EntrustServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -185,6 +186,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         'Entrust'   => Mindscms\Entrust\EntrustFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
