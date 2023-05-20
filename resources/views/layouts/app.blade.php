197b170beede8bac108dfe9b1a0996f4e17450c5
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-05-20 09:34:36 +0300

    install front home page successfully

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,62 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="{{asset('assetsEnduser/images/favicon.ico')}}">
+    <link rel="apple-touch-icon" href="{{asset('assetsEnduser/images/icon.png')}}">
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{asset('assetsEnduser/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assetsEnduser/css/plugins.css')}}">
+    <link rel="stylesheet" href="{{asset('assetsEnduser/css/style.css')}}">
+
+    <!-- Cusom css -->
+    <link rel="stylesheet" href="{{asset('assetsEnduser/css/custom.css')}}">
+
+    <!-- Modernizer js -->
+    <script src="{{asset('assetsEnduser/js/vendor/modernizr-3.5.0.min.js')}}"></script>
+
+
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('enduser.login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('enduser.login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('enduser.register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('enduser.register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('enduser.logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('enduser.logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="wrapper" id="wrapper">
+            @include('partials.enduser.header')
+            <main>
+                @yield('content')
+            </main>
+            @include('partials.enduser.footer')
+        </div>
     </div>
+
+{{--    <!-- Scripts -->--}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- JS Files -->
+    <script src="{{asset('assetsEnduser/js/vendor/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('assetsEnduser/js/popper.min.js')}}"></script>
+    <script src="{{asset('assetsEnduser/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assetsEnduser/js/plugins.js')}}"></script>
+    <script src="{{asset('assetsEnduser/js/active.js')}}"></script>
 </body>
-</html>
+
